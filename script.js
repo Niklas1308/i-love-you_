36c19b6e33700f9f46c2fbd4a2f4b3faf996a538
Niklas1308 <niklas.krenner13@gmail.com>
2025-01-29 12:24:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-document.getElementById("card").addEventListener("click", function() {
-    let front = document.querySelector(".card-front");
-    let inside = document.querySelector(".card-inside");
 
-    front.style.display = "none";
-    inside.style.display = "flex";
-});
-
-function answerYes() {
-    alert("Juhu! 💖 Ich freue mich!");
-}
-
-function answerNo() {
-}
